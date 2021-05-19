835b59e79eab05a05d7f453f297b617e7afbd1cc
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-05-19 22:09:09 +0200

    feat: 4º guía de quarkus; Rest-assured:comprender la estructura de los test RestAssured

--- getting-started/src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -2,9 +2,7 @@ package org.acme.getting.started;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
-
 import java.util.UUID;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -14,21 +12,21 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("hello"));
+                .when().get("/hello") //cuando recibas el endpoint /hello
+                .then()                  //entonces...
+                .statusCode(200)         //valida con los códigos de errores http
+                .body(is("hello"));  //que devuelva lo especificado
     }
 
     @Test
     public void testGreetingEndpoint() {
         String uuid = UUID.randomUUID().toString();
         given()
-                .pathParam("name", uuid)
-                .when().get("/hello/greeting/{name}")
-                .then()
-                .statusCode(200)
-                .body(is("hello " + uuid));
+                .pathParam("name", uuid)                //se utiliza para cuando en el endpoint tiene algún parámetro ('tipo', parámetro)
+                .when().get("/hello/greeting/{name}")   //cuando recibas este endpoint
+                .then()                                    //entonces...
+                .statusCode(200)                           //valida con los códigos de errores http
+                .body(is("hello " + uuid));           // que devuelva el string + el parámetro introducido
     }
 
 }
